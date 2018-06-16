a6e8d9e48a8e69647abba9ca8642640268dad26c
brn.developers <brn.developers@achiever-82.local>
2018-06-16 13:36:58 +0530

    g

---
+++ JSONImageDownload/HeroViewController.swift
@@ -0,0 +1,25 @@
+
+
+//
+//  HeroViewController.swift
+//  JSONImageDownload
+//
+//  Created by brn.developers on 6/14/18.
+//  Copyright © 2018 brn.developers. All rights reserved.
+//
+
+import UIKit
+
+class HeroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var namesLbl: UILabel!
+    @IBOutlet weak var attributesLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var LegsLbd: UILabel!
+}
